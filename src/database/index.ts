822bfb3c3b7c199bf89df0e71deaa071d3e73d7d
Victor <victorpimentel@jobspace.com.br>
2022-10-19 13:34:57 -0300

    Ajuste de conexão com o banco de dados

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,6 +3,8 @@ import { DataSource } from "typeorm";
 const myDataSource = new DataSource({
    type: "mongodb",
    host: "localhost",
+   username: "root",
+   password: "root",
    port: 27017,
    database: "rentx",
 });
